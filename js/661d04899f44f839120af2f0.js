ce20603c41b1bc8fff2a4ac6b9d19a313c518592
farish-jamal <farishjamal98@gmail.com>
2024-04-23 01:05:57 +0530

    fixess

---
+++ js/661d04899f44f839120af2f0.js
@@ -0,0 +1,3 @@
+function loadJS(){let scriptEle=document.createElement("script");scriptEle.setAttribute("src","https://dul5jcc85arql.cloudfront.net/partners/js/main/661d04899f44f839120af2f0.js")
+scriptEle.setAttribute("type","text/javascript");scriptEle.setAttribute("defer",!0);document.body.appendChild(scriptEle)}
+if(document.readyState!="loading"){loadJS()}else{document.addEventListener("DOMContentLoaded",loadJS,!1)}
